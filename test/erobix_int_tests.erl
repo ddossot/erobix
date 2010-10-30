37bc38991f1fc1cd8a9825d733904b67ec3dbca9
David Dossot <david.dossot@mulesoft.com>
2010-10-30 15:56:14 -0700

    added basic integration tests on lobby and about

--- test/erobix_int_tests.erl
+++ test/erobix_int_tests.erl
@@ -29,8 +29,9 @@ start() ->
 unauthenticated_tests() ->
   etap:msg("Unauthenticated Tests"),
   get_lobby(),
+  get_about(),
   ok.
-  
+
 get_lobby() ->
   etap:msg("Lobby Tests"),
   LobbyHref = ?OBIX_SERVER_URL ++ "/",
@@ -38,11 +39,25 @@ get_lobby() ->
   LobbyDom = ensure_valid_obix_response(Result),
   
   ensure_href(LobbyHref, LobbyDom),
-  ensure_obj_of_type("obix:Lobby", LobbyDom),
+  ensure_obj_of_type("obj", "obix:Lobby", LobbyDom),
   
-  % TODO test lobby specific child elements
-  io:format("~n~1024p~n", [LobbyDom]),
+  {_, _, [AboutRef]} = LobbyDom,
+  ensure_href("about/", AboutRef),
+  ensure_obj_of_type("ref", "obix:About", AboutRef),
+  ok.
+
+get_about() ->
+  etap:msg("About Tests"),
+  AboutHref = ?OBIX_SERVER_URL ++ "/about/",
+  Result = ibrowse:send_req(AboutHref, [], get),
+  AboutDom = ensure_valid_obix_response(Result),
   
+  ensure_href(AboutHref, AboutDom),
+  ensure_obj_of_type("obj", "obix:About", AboutDom),
+  
+  {_, _, [ObixVersionStr|_]} = AboutDom,
+  ensure_href("obixVersion/", ObixVersionStr),
+  ensure_obj_of_type("str", ObixVersionStr),
   ok.
 
 ensure_valid_obix_response({_, _, Headers, Body}) ->
@@ -59,15 +74,25 @@ ensure_valid_obix_response({_, _, Headers, Body}) ->
   {ok, Dom, _} = erlsom:simple_form(Body),
   Dom.
 
-ensure_href(ExpectedHref, {_, Attributes, _}) ->
+ensure_href(ExpectedHref, {ActualElement, Attributes, _}) ->
   etap:is(proplists:get_value("href", Attributes),
           ExpectedHref,
-          "@href is correct"),
+          "@href is correct on " ++ ActualElement),
   ok.  
-  
-ensure_obj_of_type(ExpectedType, {"{http://obix.org/ns/schema/1.0}obj", Attributes, _}) ->
+
+ensure_obj_of_type(ExpectedElement, Actual) ->
+  ensure_obj_of_type(ExpectedElement, undefined, Actual),
+  ok.
+
+ensure_obj_of_type(ExpectedElement,
+                   ExpectedType,
+                   {ActualElement = "{http://obix.org/ns/schema/1.0}" ++ ExpectedElement, Attributes, _}) ->
+                   
   etap:is(proplists:get_value("is", Attributes),
           ExpectedType,
-          "@is is correct"),
+          "@is is correct on " ++ ActualElement),
+  ok;
+ensure_obj_of_type(ExpectedElement, _, Actual) ->
+  etap:is(ExpectedElement, Actual, "correct xml element"),
   ok.
 
